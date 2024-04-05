5bf7f00aa5d55b2d92b0afed1ff0d62782270e4e
cuonglexuan203 <cuonglexuan203@gmail.com>
2024-04-06 03:07:32 +0700

    update module show code

--- src/app/code/page.tsx
+++ src/app/code/page.tsx
@@ -1,13 +1,18 @@
+'use client'
 import Image from 'next/image';
 import React from 'react'
+import Spline from '@splinetool/react-spline';
 
 const AdapterPatternCodePage = () => {
     return (
-        <div className="grid grid-cols-2">
-            <Image alt="image" src={"/code/ios.png"} sizes="100%" width={0} height={0} className="w-full h-screen" />
-            <Image alt="image" src={"/code/android.png"} sizes="100%" width={0} height={0} className="w-full h-screen" />
-            <Image alt="image" src={"/code/adapter.png"} sizes="100%" width={0} height={0} className="w-full h-screen" />
-            <Image alt="image" src={"/code/main.png"} sizes="100%" width={0} height={0} className="w-full h-screen" />
+        // <div className="grid grid-cols-2">
+        //     <Image alt="image" src={"/code/ios.png"} sizes="100%" width={0} height={0} className="w-full h-screen" />
+        //     <Image alt="image" src={"/code/android.png"} sizes="100%" width={0} height={0} className="w-full h-screen" />
+        //     <Image alt="image" src={"/code/adapter.png"} sizes="100%" width={0} height={0} className="w-full h-screen" />
+        //     <Image alt="image" src={"/code/main.png"} sizes="100%" width={0} height={0} className="w-full h-screen" />
+        // </div>
+        <div>
+            <Spline scene="https://prod.spline.design/qcrdZxr3j55yQNc9/scene.splinecode" />
         </div>
     )
 }
--- src/app/component/Android.tsx
+++ src/app/component/Android.tsx
@@ -7,10 +7,6 @@ const Android = () => {
                 frameBorder="0"
                 allowFullScreen
                 allow="autoplay; fullscreen; xr-spatial-tracking"
-                xr-spatial-tracking
-                execution-while-out-of-viewport
-                execution-while-not-rendered
-                web-share
                 src="https://sketchfab.com/models/4bcf668cb8974866966a9150d05634d2/embed?autostart=1&preload=1&ui_hint=0"
             >
             </iframe>
